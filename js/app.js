0b06765995f02b2ef214fde4c8ae4a2c1b90ed2f
KISACHI <lightning_x6@yahoo.co.jp>
2026-02-07 20:06:44 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-
+// ===== Supabase 初期化（app.jsの先頭）=====
 const SUPABASE_URL = "https://vhnshcsfucefikbxhuxx.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_ZQNSoFRd8Yhw5Qt6WBM2HA_nEjb1214";
 
