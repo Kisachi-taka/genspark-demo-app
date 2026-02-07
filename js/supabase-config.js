d206dfdcaac54dc10bec03fe99b5a505b68a9b43
KISACHI <lightning_x6@yahoo.co.jp>
2026-02-07 19:18:17 +0900

    Create supabase-config.js

---
+++ js/supabase-config.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://vhnshcsfucefikbxhuxx.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_ZQNSoFRd8Yhw5Qt6WBM2HA_nEjb1214";
+
+window._supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
